daf089879e4fe2db52f39d6190d7cd59e71fc3d7
Jessica Murphy <jessicaammurphy@gmail.com>
2025-03-05 19:48:08 -0500

    fixed player

--- Gamedev/Assets/scripts/Player.cs
+++ Gamedev/Assets/scripts/Player.cs
@@ -1,35 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+public class Player : MonoBehaviour
+{
 
-
-public class Player : MonoBehaviour {
-
-    
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
 
-
     private bool isWalking;
+    private Rigidbody rb;
 
-    private void Update()
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * Time.deltaTime * moveSpeed;
-
         isWalking = moveDir != Vector3.zero;
 
-        //visual
-        float rotateSpeed = 10f;
-        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+        // Move the player using Rigidbody
+        if (moveDir != Vector3.zero)
+        {
+            // Normalize the move direction to prevent faster diagonal movement
+            moveDir.Normalize();
 
+            // Apply movement using Rigidbody
+            rb.velocity = moveDir * moveSpeed;
 
-        
-        
-        
+            // Rotate the player smoothly
+            float rotateSpeed = 10f;
+            transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.fixedDeltaTime * rotateSpeed);
+        }
+        else
+        {
+            // Stop movement if no input
+            rb.velocity = Vector3.zero;
+        }
     }
 
     public bool IsWalking()
